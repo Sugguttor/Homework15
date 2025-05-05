6fa0062a565a7a99eccc01eae1d7c0557b82cb5b
Sugguttor <antonofmixa@mail.ru>
2025-05-05 18:59:34 +0300

    Домашнее задание про мапы, корректировка.

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -6,7 +6,7 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.*;
 
 public class ProductBasket {
-    private final Map<String, LinkedList<Product>> productBasket = new TreeMap<>();
+    private final Map<String, LinkedList<Product>> productBasket = new HashMap<>();
 
     public void addProduct(Product product) {
         productBasket.computeIfAbsent(product.getName(), k -> new LinkedList<>()).add(product);
@@ -15,59 +15,58 @@ public class ProductBasket {
     public double calculateSumOfBasket() {
         double sum = 0;
         for (Map.Entry<String, LinkedList<Product>> product : productBasket.entrySet()) {
-            if (product != null) {
-                for (Product p : product.getValue())
-                    sum += p.getPrice();
+            for (Product p : product.getValue()) {
+                sum += p.getPrice();
             }
         }
         return sum;
-    }
+}
 
-    public void printProductBasket() {
-        double sum = calculateSumOfBasket();
-        int specialCounter = 0;
-        for (Map.Entry<String, LinkedList<Product>> product : productBasket.entrySet()) {
-            System.out.println(product.toString());
-            for (Product p : product.getValue()) {
-                if (p.isSpecial()) {
-                    specialCounter++;
-                }
+public void printProductBasket() {
+    double sum = calculateSumOfBasket();
+    int specialCounter = 0;
+    for (Map.Entry<String, LinkedList<Product>> product : productBasket.entrySet()) {
+        System.out.println(product.toString());
+        for (Product p : product.getValue()) {
+            if (p.isSpecial()) {
+                specialCounter++;
             }
         }
-        if (sum == 0) {
-            System.out.println("в корзине пусто");
-        } else {
-            System.out.println("Итого: " + sum);
-            System.out.println("Специальных товаров: " + specialCounter);
-        }
     }
+    if (sum == 0) {
+        System.out.println("в корзине пусто");
+    } else {
+        System.out.println("Итого: " + sum);
+        System.out.println("Специальных товаров: " + specialCounter);
+    }
+}
 
-    public boolean searchProduct(String name) {
-        for (Map.Entry<String, LinkedList<Product>> product : productBasket.entrySet()) {
-            for (Product p : product.getValue()) {
-                if (p.getName().equalsIgnoreCase(name)) {
-                    return true;
-                }
+public boolean searchProduct(String name) {
+    for (Map.Entry<String, LinkedList<Product>> product : productBasket.entrySet()) {
+        for (Product p : product.getValue()) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                return true;
             }
         }
-        return false;
     }
+    return false;
+}
 
-    public void clear() {
-        productBasket.clear();
-    }
+public void clear() {
+    productBasket.clear();
+}
 
-    public LinkedList<Product> removeThisProduct(String name) {
-        LinkedList<Product> removedProducts = new LinkedList<>();
-        if (productBasket.containsKey(name)) {
-            removedProducts.addAll(productBasket.get(name));
-            productBasket.remove(name);
-        }
-         if (removedProducts.isEmpty()) {
-            System.out.println("Список пуст");
-        }
-        return removedProducts;
+public LinkedList<Product> removeThisProduct(String name) {
+    LinkedList<Product> removedProducts = new LinkedList<>();
+    if (productBasket.containsKey(name)) {
+        removedProducts.addAll(productBasket.get(name));
+        productBasket.remove(name);
     }
+    if (removedProducts.isEmpty()) {
+        System.out.println("Список пуст");
+    }
+    return removedProducts;
+}
 
 
 }
--- src/org/skypro/skyshop/search/SearchEngine.java
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -1,10 +1,11 @@
 package org.skypro.skyshop.search;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.TreeMap;
 
 public class SearchEngine {
-    private final LinkedList<Searchable> searchableItems;
+    private final List<Searchable> searchableItems;
 
     public SearchEngine() {
         searchableItems = new LinkedList<>();
