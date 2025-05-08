d59e6e1785e93a0835971d4f6f24b07430d4df70
Sugguttor <antonofmixa@mail.ru>
2025-05-08 11:07:18 +0300

    Java Collections Framework: Set. Correction.

--- src/org/skypro/skyshop/search/SearchEngine.java
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -19,9 +19,9 @@ public class SearchEngine {
             public int compare(Searchable o2, Searchable o1) {
                 int lengthCompare = Integer.compare(o2.getName().length(), o1.getName().length());
                 if (lengthCompare == 0) {
-                    return lengthCompare;
+                    return o2.getName().compareToIgnoreCase(o1.getName());
                 }
-                return o2.getName().compareToIgnoreCase(o1.getName());
+                return lengthCompare;
             }
         });
         for (Searchable item : searchableItems) {
